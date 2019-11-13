0ea98a707e011ba427bba6c08575922d65c1cec9
Barreto <v.barreto42@outlook.com>
2019-11-13 08:03:34 -0300

    Style inclusion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Provider } from "mobx-react";
 import store from "../src/store";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import routes from "../src/config/routes";
+import "./style/app.css";
+
 class App extends Component {
   render() {
     return (
