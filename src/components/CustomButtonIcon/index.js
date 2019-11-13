262df28b15c0d22f3a8a1095045fe78b278b119e
Barreto <v.barreto42@outlook.com>
2019-11-13 09:39:01 -0300

    Including redirection behavion at received OnClick

--- src/components/CustomButtonIcon/index.js
+++ src/components/CustomButtonIcon/index.js
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import "../../style/CustomButtonIcon.css";
+import { baseUrl } from "../../config/location";
 
 class CustomButtonIcon extends React.Component {
     render() {
-        const {icon, text} = this.props;
+        const { icon, text, onClick } = this.props;
+        
+        const customOnClick = () => {
+            onClick();
+            window.location = `${baseUrl}filters`;
+        }
+
         return (
-            <button className='custom-btn-icon-text'>
+            <button
+                onClick={customOnClick}
+                className='custom-btn-icon-text'
+            >
                 <span className='custom-btn-icon'>{icon}</span>
                 <span className='custom-btn-text'>{text}</span>
             </button>
