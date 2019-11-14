73860bf500a6d453e54f0f4832171c91e170739d
Barreto <v.barreto42@outlook.com>
2019-11-13 21:31:17 -0300

    Adding FipeStore to RootStore

--- src/store/RootStore.js
+++ src/store/RootStore.js
@@ -1,11 +1,13 @@
-import { configure } from 'mobx'
-import UserStore from './UserStore'
-import UIStore from './UIStore'
+import { configure } from 'mobx';
+import UserStore from './UserStore';
+import UIStore from './UIStore';
+import FipeStore from './FipeStore';
 
 export default class RootStore {
   constructor() {
-    configure({ enforceActions: 'always' })
-    this.userStore = new UserStore()
-    this.uiStore = new UIStore()
+    configure({ enforceActions: 'always' });
+    this.userStore = new UserStore();
+    this.uiStore = new UIStore();
+    this.fipeStore = new FipeStore();
   }
 }
