03a36139c71aa22ab5a553a8b0cb41b2318c83b8
Barreto <v.barreto42@outlook.com>
2019-11-14 21:34:23 -0300

    Include loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loading(props) {
+    return (
+        <div className='loading-container'>
+            <div className='mobi-loading' />
+        </div>
+    );
+}
