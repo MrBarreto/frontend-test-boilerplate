52f6913b251879418452aeaf5522ccf614ce07ce
Barreto <v.barreto42@outlook.com>
2019-11-14 21:37:35 -0300

    Exporting components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButtonIcon from "./CustomButtonIcon/index";
+import DropDownList from "./DropDownList/index";
+import Loading from "./Loading/index";
+import RoundedButton from "./RoudendButton/index";
+import Logo from "./Logo/index";
 
-export { CustomButtonIcon };
+export { CustomButtonIcon, DropDownList, Loading, RoundedButton, Logo };
